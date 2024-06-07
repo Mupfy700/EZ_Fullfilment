74ae79cff3d0aa49c9ab4bcee328c0587c27c366
Mupfy700 <65909034+Mupfy700@users.noreply.github.com>
2024-06-07 11:47:05 +0200

    refactor: Improve CSV file reading in process_orders.py

--- scripts/run_fullfilment.py
+++ scripts/run_fullfilment.py
@@ -14,7 +14,7 @@ def main():
         print("Eingaben nicht vollständig. Abbruch.")
         return
 
-    weight_map = {1: 0.1, 2: 0.2, 3: 0.2, 4: 0.2, 5: 0.3, 6: 0.3, 7: 0.3, 8: 0.3, 9: 0.4, 10: 0.4, 11: 0.4, 12: 0.4, 13: 0.4, 14: 0.4, 15: 0.4, 16: 0.5, 17: 0.7, 18: 0.7, 19: 0.7, 20: 0.7}
+    weight_map = {1: 0.1, 2: 0.2, 3: 0.2, 4: 0.2, 5: 0.2, 6: 0.3, 7: 0.3, 8: 0.3, 9: 0.3, 10: 0.4, 11: 0.4, 12: 0.5, 13: 0.5, 14: 0.5, 15: 0.5, 16: 0.6, 17: 0.7, 18: 0.7, 19: 0.7, 20: 0.7, 21: 0.8, 22: 0.8, 23: 0.8, 24: 0.8}
 
     file_manager = FileManager(input_folder, output_folder, weight_map)
     file_manager.process_files(specific_name)
